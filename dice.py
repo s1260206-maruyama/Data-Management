fd882c6e4c14f56472c6600deab8f6919544895d
s1260206-maruyama <s1260206@u-aizu.ac.jp>
2019-07-11 20:19:51 +0900

    problem phase 2

---
+++ dice.py
@@ -0,0 +1,9 @@
+import random
+
+dice1=random.randint(1,6)
+dice2=random.randint(1,6)
+
+print("Rolling the dice...")
+print('Die 1: %d' % dice1)
+print('Die 2: %d' % dice2)
+print('Total value: %d' % (dice1+dice2))
