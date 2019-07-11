95abd2348673e287ba61bdb6a5d036669fc45fcb
s1260206-maruyama <s1260206@u-aizu.ac.jp>
2019-07-11 20:31:54 +0900

    problem phase 5

--- dice.py
+++ dice.py
@@ -2,8 +2,14 @@ import random
 
 dice1=random.randint(1,6)
 dice2=random.randint(1,6)
+total=dice1+dice2
 
 print("Rolling the dice...")
 print('Die 1: %d' % dice1)
 print('Die 2: %d' % dice2)
-print('Total value: %d' % (dice1+dice2))
+print('Total value: %d' % total)
+
+if total>7:
+    print("You won!") 
+else:
+    print("You lost!")
